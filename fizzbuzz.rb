7ab7bdadf8d5cd40a40e0b46e8a98f5855f37e18
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-11 03:45:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-define fizzbuzz(input)
-  if input%3 == 0 && input%5 == 0
+def fizzbuzz(x)
+  if x%3 == 0 && x%5 == 0
     return "fizzbuzz"
-  elsif input%3 == 0
+  elsif x%3 == 0
     return "fizz"
-  elsif input%5 == 0
+  elsif x%5 == 0
     return "buzz"
   else
     return nil
